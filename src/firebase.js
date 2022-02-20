54b87ca209c2069abad7c31b527e621aa3e58d3c
chetan-2002 <77783033+chetan-2002@users.noreply.github.com>
2022-02-20 15:21:03 +0530

    gitignore file updated

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD7v5tvLS18syltgCbBvdBs3s73CStaroc",
-  authDomain: "local-assist-a56d7.firebaseapp.com",
-  projectId: "local-assist-a56d7",
-  storageBucket: "local-assist-a56d7.appspot.com",
-  messagingSenderId: "1054881707423",
-  appId: "1:1054881707423:web:ecf0f3257d5d7c82f179aa"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 // Initialize Firebase
